0208550c37922cb920f19e21bc3a2dea7e80c963
Akshay <akshaynena1243@gmail.com>
2024-06-28 13:19:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById('password');
+const length = 10;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_/\{}[]-";
+
+const allChars =  upperCase +  lowerCase + number + symbol;
+function createpassword(){
+    let password = '';
+    password = password+upperCase[Math.floor(Math.random()*upperCase.length)];
+    password = password+lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password = password+number[Math.floor(Math.random()*number.length)];
+    password = password+symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(password.length<length){
+         password = password+allChars[Math.floor(Math.random()*allChars.length)]
+    }
+    passwordBox.value = password;
+}
+function copybox(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
